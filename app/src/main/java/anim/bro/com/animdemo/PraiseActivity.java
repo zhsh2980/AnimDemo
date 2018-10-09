a2147dfb74e438eaf79aca041ac1f93cfd2dabd2
shanz01 <shanz01@jumei.com>
2018-10-09 11:54:40 +0800

    add praise

---
+++ app/src/main/java/anim/bro/com/animdemo/PraiseActivity.java
@@ -0,0 +1,13 @@
+package anim.bro.com.animdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PraiseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_praise);
+    }
+}
