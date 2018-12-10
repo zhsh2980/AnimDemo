8b3b4db66479e4df6c0b9ccc864ca4b70a66696f
shanz01 <shanz01@jumei.com>
2018-12-10 21:30:33 +0800

    add auto hide

---
+++ app/src/main/java/anim/bro/com/animdemo/bean/TestBean.java
@@ -0,0 +1,7 @@
+package anim.bro.com.animdemo.bean;
+
+/**
+ * Created by zhangshan on 2018/12/10 17:00.
+ */
+public class TestBean {
+}
---
+++ app/src/main/java/anim/bro/com/animdemo/bean/TestBean2.java
@@ -0,0 +1,10 @@
+package anim.bro.com.animdemo.bean;
+
+/**
+ * Created by zhangshan on 2018/12/10 17:00.
+ */
+public class TestBean {
+
+
+
+}
